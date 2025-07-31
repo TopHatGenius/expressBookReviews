0a6f6b79ef16f199d7abfad4b569288d441b6a00
TopHatGenius <bezerker87@gmail.com>
2025-07-31 18:35:45 -0400

    Up to task 1

--- final_project/index.js
+++ final_project/index.js
@@ -10,8 +10,22 @@ app.use(express.json());
 
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
+//Updated middleware to /customer/auth/* authentication code (Double check)
 app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+    if (req.session.authorization) {
+        let token = req.session.authorization['accessToken'];
+
+        jwt.verify(token, "access", (err, user) => {
+            if (!err) {
+                req.user = user;
+                next();
+            } else {
+                return res.status(403).json({message: "Unauthenticated user"});
+            }
+        });
+    } else {
+        return res.status(403).json({message: "User not logged in"});
+    }
 });
  
 const PORT =5000;
