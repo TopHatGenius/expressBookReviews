80a7c8e1ca0c911958a6a76c87044d145cd6b3ca
TopHatGenius <bezerker87@gmail.com>
2025-08-07 18:37:00 -0400

    Up to task 6, booksdb modified to show a proper description

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -40,16 +40,33 @@ public_users.get('/author/:author',function (req, res) {
    }
 });
 
-// Get all books based on title
+// Get all books based on title - Task 4
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const title = req.params.title;
+  const titleBooks = [];
+
+  for (let bookID in books) {
+    if (books[bookID].title === title) {
+        titleBooks.push(books[bookID]);
+    }
+  }
+
+  if (titleBooks.length > 0) {
+    return res.status(200).json(titleBooks);
+  } else {
+    return res.status(404).json({message: "Title not found"});
+  }
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const isbn = req.params.isbn;
+
+    if  (books[isbn]) {
+        return res.status(200).json(books[isbn].reviews);
+    } else {
+        return res.status(404).json({message: "Book not found"});
+    }
 });
 
 module.exports.general = public_users;
