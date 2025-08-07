6bb5a203e6620cba3c857e06bc4d81977ae4ee55
TopHatGenius <bezerker87@gmail.com>
2025-08-07 15:34:23 -0400

    Up to task 3

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -10,15 +10,16 @@ public_users.post("/register", (req,res) => {
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
-// Task 1 inprog
+// Task 1 inprog - DONE it seems
 public_users.get('/',function (req, res) {
     res.send(JSON.stringify(books,null,4));
 });
 
-// Get book details based on ISBN
+// Get book details based on ISBN - Task 2 - DONE
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  
+  return res.status(200).json(books[isbn]);
  });
   
 // Get book details based on author
